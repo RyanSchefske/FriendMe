6a3a96f551ffa61c9bac144c9956029f4a95e6e1
Ryan Schefske <ryanschefske@gmail.com>
2018-05-23 09:36:55 -0500

    Initial Commit

---
+++ FriendMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendMe
+//
+//  Created by Ryan Schefske on 5/23/18.
+//  Copyright © 2018 Ryan Schefske. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
